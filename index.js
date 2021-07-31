b43d220d79f0c70b38e1258f91143605d4207012
Meghajit Mazumdar <Megh319@gmail.com>
2021-08-01 04:15:25 +0530

    Add code to generate pdf
    - use landscape mode to fix issues where some
    content gets hidden on the top of the page due to the
    header being too thick
    - some sites don't render the content unless scrolled.
    Hence, we scroll till end of page for 10ms to ensure all
    the network calls happen. This could be made configurable.
    - use incognito chrome in headless mode to prohibit
    cookie persistence
    - use A4 print format

---
+++ index.js
@@ -0,0 +1,33 @@
+const puppeteer = require('puppeteer');
+
+async function scrollToEndOfPage(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeightScrolled = 0;
+            var distanceToScrollPerIteration = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distanceToScrollPerIteration);
+                totalHeightScrolled += distanceToScrollPerIteration;
+
+                if(totalHeightScrolled >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 10);
+        });
+    });
+}
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
+  await page.goto('https://en.wikipedia.org/wiki/Papyrus', {
+    waitUntil: 'networkidle2',
+  });
+  await page.emulateMediaType('screen');
+  await autoScroll(page);
+  await page.pdf({path: 'webpage.pdf', format: 'a4', preferCSSPageSize: true, landscape: true});
+  await browser.close();
+  })();
