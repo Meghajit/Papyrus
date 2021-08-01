fbf56807adbe697ab2291759b2ab46348028a67c
Meghajit Mazumdar <Megh319@gmail.com>
2021-08-01 21:58:50 +0530

    Refactor into node modules

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-const puppeteer = require('puppeteer');
+const pdfGenerator = require('./src/pdf-generator');
 
-async function scrollToEndOfPage(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeightScrolled = 0;
-            var distanceToScrollPerIteration = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distanceToScrollPerIteration);
-                totalHeightScrolled += distanceToScrollPerIteration;
-
-                if(totalHeightScrolled >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 10);
-        });
-    });
-}
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const context = await browser.createIncognitoBrowserContext();
-  const page = await context.newPage();
-  await page.goto('https://en.wikipedia.org/wiki/Papyrus', {
-    waitUntil: 'networkidle2',
-  });
-  await page.emulateMediaType('screen');
-  await scrollToEndOfPage(page);
-  await page.pdf({path: 'webpage.pdf', format: 'a4', preferCSSPageSize: true, landscape: true});
-  await browser.close();
-  })();
+pdfGenerator.generate('https://expressjs.com/en/changelog/4x.html', 'webpage.pdf')
+.then(result => console.log("Success: " + result))
+.catch(err => console.log("Error: " + err));
