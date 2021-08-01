0df993d7b73d25ebeaec27c8f54ab7845d98a12f
Meghajit Mazumdar <Megh319@gmail.com>
2021-08-01 22:47:16 +0530

    Add express http server

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const pdfGenerator = require('./src/pdf-generator');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-pdfGenerator.generate('https://expressjs.com/en/changelog/4x.html', 'webpage.pdf')
-.then(result => console.log("Success: " + result))
-.catch(err => console.log("Error: " + err));
+const port = 3000;
+
+app.post('/pdf', (req, res) => {
+    pdfGenerator.generate(req.body.webURL, 'webpage.pdf')
+        .then(result => {
+            console.log("Success: " + result);
+            res.sendFile('webpage.pdf',
+                {'root' : __dirname},
+                function (err) {
+                  if (err) {
+                    console.log("Error in sending file:" + err);
+                  } else {
+                    console.log("File sent");
+                  }
+                });
+        })
+        .catch(err => res.sendStatus(500));
+});
+
+app.listen(port, () => {
+  console.log(`Papyrus listening at http://localhost:${port}`);
+});
